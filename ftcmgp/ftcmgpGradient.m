cb7d36da5eb49a6d4f7ea4538835479e7d4f03f1
cdguarnizo <cdguarnizo@gmail.com>
2017-09-03 18:49:04 -0500

    Added major modifications

--- ftcmgp/ftcmgpGradient.m
+++
@@ -1,8 +0,0 @@
-function g = ftcmgpGradient(params, model)
-
-% FTCMULTIGPGRADIENT 
-% FTCMULTIGP
-
-model = ftcmgpExpandParam(model, params);
-model = ftcmgpComputeKernel(model);
-g = -ftcmgpLowerBoundGradients(model);
--- ftcmgp/ftcmgpObjective.m
+++
@@ -1,8 +0,0 @@
-function f = ftcmgpObjective(params, model)
-
-% IBPMULTIGPOBJECTIVE Wrapper function for MODELOPTIMISE objective.
-
-% IBPMULTIGP
-model = ftcmgpExpandParam(model, params);
-model = ftcmgpComputeKernel(model); %Update Kff
-f = -ftcmgpLowerBound(model);
